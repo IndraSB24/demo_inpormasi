6e7598542fcc2e62bdff9faec9ce38a3631c2c5f
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-09-24 20:17:42 +0700

    indrasb all

--- app/Controllers/Project.php
+++ app/Controllers/Project.php
@@ -39,10 +39,10 @@ class Project extends BaseController
 			'chart_pt_engineering_actual' => 30,
 			'chart_pt_procurement_plan' => $this->Model_doc_procurement->getCumDataPlanPerToday(),
 			'chart_pt_procurement_actual' => $this->Model_doc_procurement->getCumDataActualPerToday(),
-			'engineering_wf' => $data_project['engineering_wf'],
-			'procurement_wf' => $data_project['procurement_wf'],
-			'construction_wf' => $data_project['construction_wf']
-		];
+			'engineering_wf' => $data_project->engineering_wf,
+			'procurement_wf' => $data_project->procurement_wf,
+			'construction_wf' => $data_project->construction_wf
+		];		
 
 		$data = [
 			'title_meta' => view('partials/title-meta', ['title' => 'Project Dashboard']),
