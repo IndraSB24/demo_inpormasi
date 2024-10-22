b7179b3e1253999591f181439b954d70cfbf1337
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-10-22 21:33:00 +0700

    indrasb comm

--- app/Controllers/Project_detail_engineering.php
+++ app/Controllers/Project_detail_engineering.php
@@ -131,8 +131,8 @@ class Project_detail_engineering extends BaseController
                 'dataActualCum' => $getScurveDataActualCum
             ],
             'progressChartData' => [
-                'percent_plan' => $this->doc_engineering_model->getCumDataPlanPerToday($project_id),
-                'percent_actual' => $this->doc_engineering_model->getCumDataActualPerToday($project_id)
+                'percent_plan' => $this->doc_engineering_model->getCumDataPlanPerToday($project_id, $weekStartDate),
+                'percent_actual' => $this->doc_engineering_model->getCumDataActualPerToday($project_id, $weekStartDate)
             ],
             // 'progressChartData' => [
 			// 	'percent_plan' => [(object) ['cum_progress_plan' => 67.8910]],
